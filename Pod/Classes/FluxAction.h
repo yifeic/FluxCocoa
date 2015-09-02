0b347030bea664a7cd1fb5f34c97e7e8c878bdbe
yifeic <yifei.chen@outlook.com>
2015-09-02 13:55:23 -0400

    add comment

--- Pod/Classes/FluxAction.h
+++ Pod/Classes/FluxAction.h
@@ -11,6 +11,12 @@
 
 typedef id(^FluxActionBlock)(id input);
 
+/**
+ *  Inspired by Flux action from Facebook.
+ *  Action can be created by +action or +actionWithBlock:
+ *  If action is created by +action, whenever 'execute:' is called, 'executionSignal' will send a next with value 'input'.
+ *  +actionWithBlock: allows you to provide a block. 'executionSignal' will send whatever returned by the block.
+ */
 @interface FluxAction : NSObject
 
 + (instancetype)action;
